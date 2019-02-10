6e389b94e522a7b18ed81ca484418ea1fdc78c63
Florent Carrez <florent.carrez@epita.fr>
2019-02-10 19:56:49 +0100

    Push: opt r and opt t done

--- push/push.sh
+++ push/push.sh
@@ -9,18 +9,37 @@ if [[ `cat .__tmp_push__/error` != "" ]]; then
 	echo "\033[31;1;5m!\tMAKE FAILE\t!\033[0m"
 	#make
 else
-	echo ""
-	echo "\033[32;1m\tCompile OK\033[0m"
-	git push
-	git push epita
-	if [[ $# -eq 2 ]]; then
-		git tag "$1"
-	else
-		echo "\n\033[1;4mTag\033[0m: "
-		read my_tag
-		git tag "$my_tag"
+	clear
+	resume="$GREEN$BOLD\tCompile OK$NORMAL"
+	if [[ testsuit != "" ]]; then
+		make check 2>.__tmp_push__/error
+		clear
+		make check
+		if [[ `cat .__tmp_push__/error` != "" ]]; then
+			resume="$resume\n$RED$BOLD$BLINKING!\tMAKE CHECK FAIL \t!$NORMAL"
+			if [[ $testsuit == "N" ]]; then
+				echo "\n$resume"
+				exit 2
+			fi
+		else
+			resume="$resume\n$GREEN$BOLD\tmake check OK$NORMAL"
+			testsuit="OK"
+		fi	
 	fi
-	git push --tags
-	git push epita
+	if [[ $testsuit == "OK" || $testsuit == "" || $testsuit == "Y" ]]; then
+		clear
+		echo "$resume"
+		git push
+		git push "$remote"
+		if [[ $# -eq 2 ]]; then
+			git tag "$1"
+		else
+			echo "\n$BOLD$UNDERLINE Tag$NORMAL: "
+			read my_tag
+			git tag "$my_tag"
+		fi
+		git push --tags
+		git push "$remote" --tags
+	fi
 fi
 rm -r .__tmp_push__
