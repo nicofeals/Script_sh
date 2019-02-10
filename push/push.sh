e627ab1225c1cd603ee2790b658864f9c7266103
Florent Carrez <florent.carrez@epita.fr>
2019-02-10 19:35:50 +0100

    Push: opt_t: change options set for tag

--- push/push.sh
+++ push/push.sh
@@ -13,38 +13,49 @@ testsuit=""
 tag=""
 
 for arg in $@; do
-	if [[ $arg == "-r" ]]; then
+	if [[ $arg == "-r" && $remote == "" ]]; then
 		opt_kind="remote"
+
+	elif [[ $arg == "-t" && $testsuit == "" ]]; then
+		opt_kind="testsuit"
+		testsuit="N"
+
+	elif [[ $arg == "--tag" && $tag == "" ]]; then
+		opt_kind="tag"
 
 	elif [[ $opt_kind == "remote" ]]; then
 		remote="$arg"
 		opt_kind=""
 
-	elif [[ $arg == "-t" ]]; then
-		opt_kind="testsuit"
-
 	elif [[ $opt_kind == "testsuit" ]]; then
 		if [ $arg != "N" -a $arg != "Y" ]; then
-			echo "Fail Usage: ./push [-r <remote_name>=epita][-t <continue>=N][tagname]" >&2
+			echo "Fail Usage: ./push [-r <remote_name>][-t <continue>=N][--tag <tagname>]" >&2
 			echo "option -t can be followed by Y or N and is set to N by default" >&2
 		else
 			testsuit=$arg
 		fi
 		opt_kind=""
 
-	elif [[ opt_kind == "" ]]; then
+	elif [[ opt_kind == "tag" ]]; then
 		tag="$arg"
-		opt_kind="none"
+		opt_kind=""
 
-	elif [[ opt_kind == "none" ]]; then
-		echo "$arg is not an option and tagname is allready set to be: $tag"
-		echo "may be you want change the tagname ? [y/n]"
-		read answer
-		if [[ answer == "y" ]]; then
-			tag=$arg
-		else
-			echo "Fail Usage: ./push [-r <remote_name>=epita][-t <continue>=N][tagname]" >&2
+	else
+		echo "Fail Usage: ./push [-r remote_name][-t <continue>=N][--tag <tagname>]\n" >&2
+		echo "$arg is not an option or is allready set" >&2
+		echo "here are the options allready set:"
+		if [[ $remote != "" ]]; then
+			echo "-r : $remote"
 		fi
+
+		if [[ $testsuit != "" ]]; then
+			echo "-t : $testsuit"
+		fi
+
+		if [[ $tag != "" ]]; then
+			echo "--tag : $tag"
+		fi
+		
 	fi
 done
 mkdir .__tmp_push__
