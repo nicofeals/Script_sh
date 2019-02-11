cce7b073a025b7ab049f0dc92074df3d3f2916c5
Florent Carrez <florent.carrez@epita.fr>
2019-02-11 22:16:18 +0100

    Push: try [ instead of [[ for if

--- push/push.sh
+++ push/push.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 check_opt_r() {
-	if [[ $opt_kind == "-r" ]]; then
+	if [ $opt_kind == "-r" ]; then
 		echo "Fail Usage: $Usage"
 		echo "$arg can not follow $opt_kind"
 		exit 3
@@ -9,7 +9,7 @@ check_opt_r() {
 }
 
 check_opt_tag() {
-	if [[ $opt_kind == "--tag" ]]; then
+	if [ $opt_kind == "--tag" ]; then
 		echo "Fail Usage: $Usage"
 		echo "$arg can not follow $opt_kind"
 		exit 3
@@ -44,37 +44,37 @@ force=0
 	# -h        :    display that help"
 
 for arg in $@; do
-	if [[ $arg == "-h" || $arg == "--help" ]]; then
+	if [ $arg == "-h" || $arg == "--help" ]; then
 		check_opt_r
 		check_opt_tag
 		help="$arg"
 
-	elif [[ $arg == "-f" ]]; then
+	elif [ $arg == "-f" ]; then
 		check_opt_r
 		check_opt_tag
 		force=1
 
-	elif [[ $arg == "-r" && $remote == "" ]]; then
+	elif [ $arg == "-r" && $remote == "" ]; then
 		check_opt_r
 		check_opt_tag
 		opt_kind="-r"
 
-	elif [[ $arg == "-t" && $testsuit == "" ]]; then
+	elif [ $arg == "-t" && $testsuit == "" ]; then
 		check_opt_r
 		check_opt_tag
 		opt_kind="-t"
 		testsuit="N"
 
-	elif [[ $arg == "--tag" && $tag == "" ]]; then
+	elif [ $arg == "--tag" && $tag == "" ]; then
 		check_opt_r
 		check_opt_tag
 		opt_kind="--tag"
 
-	elif [[ $opt_kind == "-r" ]]; then
+	elif [ $opt_kind == "-r" ]; then
 		remote="$arg"
 		opt_kind=""
 
-	elif [[ $opt_kind == "-t" ]]; then
+	elif [ $opt_kind == "-t" ]; then
 		if [ $arg != "N" -a $arg != "Y" ]; then
 			echo "Fail Usage: $Usage" >&2
 			echo "option -t can be followed by Y or N and is set to N by default" >&2
@@ -84,9 +84,9 @@ for arg in $@; do
 		fi
 		opt_kind=""
 
-	elif [[ $opt_kind == "--tag" ]]; then
-		if [[ ${arg:0:1} == "-" ]]; then
-			if [[ $arg == "-no-tag" ]]; then
+	elif [ $opt_kind == "--tag" ]; then
+		if [ ${arg:0:1} == "-" ]; then
+			if [ $arg == "-no-tag" ]; then
 				tag="-no-arg"
 			else
 				echo "$arg is not a valid tag.">&2
@@ -101,24 +101,24 @@ for arg in $@; do
 		echo "Fail Usage: $Usage\n" >&2
 		echo "$arg is not an option or is allready set" >&2
 		echo "here are the options used:"
-		if [[ $remote != "" ]]; then
+		if [ $remote != "" ]; then
 			echo "-r : $remote"
 		fi
 
-		if [[ $testsuit != "" ]]; then
+		if [ $testsuit != "" ]; then
 			echo "-t : $testsuit"
 		fi
 
-		if [[ $tag != "" ]]; then
+		if [ $tag != "" ]; then
 			echo "--tag : $tag"
 		fi
-		if [[ $help != "" ]]; then
+		if [ $help != "" ]; then
 			echo "$help"
 		fi
 		exit 3
 	fi
 done
-if [[ $help != "" ]]; then
+if [ $help != "" ]; then
 	echo "$Usage"
     echo ""
     echo "-r        :    also push on 'remote_name'."
@@ -134,26 +134,26 @@ fi
 mkdir .__tmp_push__
 make
 make 2> .__tmp_push__/error
-if [[ `cat .__tmp_push__/error` != "" ]]; then
+if [ `cat .__tmp_push__/error` != "" ]; then
 	echo ""
 	resume="$RED$BOLD$BLINKING!\tMAKE FAILE\t!$NORMAL"
-	if [[ $force -eq 0 ]]; then
+	if [ $force -eq 0 ]; then
 		echo "$resume"
 		exit 1
 	fi
 	force=2
 fi
-if [[ $force -ne 2 ]]; then
+if [ $force -ne 2 ]; then
 	clear
 	resume="$GREEN$BOLD\tCompile OK$NORMAL"	
 fi
-if [[ $testsuit != "" ]]; then
+if [ $testsuit != "" ]; then
 	make check 2>.__tmp_push__/error
 	clear
 	make check
-	if [[ `cat .__tmp_push__/error` != "" ]]; then
+	if [ `cat .__tmp_push__/error` != "" ]; then
 		resume="$resume\n$RED$BOLD$BLINKING!\tMAKE CHECK FAIL \t!$NORMAL"
-		if [[ $testsuit == "N" ]]; then
+		if [ $testsuit == "N" ]; then
 			echo "\n$resume"
 			exit 2
 		fi
@@ -162,20 +162,20 @@ if [[ $testsuit != "" ]]; then
 		testsuit="OK"
 	fi	
 fi
-if [[ $testsuit == "OK" || $testsuit == "" || $testsuit == "Y" ]]; then
+if [ $testsuit == "OK" || $testsuit == "" || $testsuit == "Y" ]; then
 	clear
 	echo "$resume"
 	git push
 
-	if [[ $tag == "" ]]; then
+	if [ $tag == "" ]; then
 		echo "\n$BOLD$UNDERLINE Tag$NORMAL: "
 		read tag
 	fi
-	if [[ $tag != "-no-tag" ]]; then
+	if [ $tag != "-no-tag" ]; then
 		git tag "$tag"
 		git push --tags	
 	fi
-	if [[ $remote != "" ]]; then
+	if [ $remote != "" ]; then
 		git push "$remote"
 		git push "$remote" --tags	
 	fi
